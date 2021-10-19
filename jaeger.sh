19f05020a60445dc28d356efeddf44b1f3e4fc57
yinyuanyuan <yinyuanyuan@52tt.com>
2021-10-19 15:32:24 +0800

    script for starting jaeger and zipkin

---
+++ jaeger.sh
@@ -0,0 +1,1 @@
+docker run --rm -d --name jaeger -p 6831:6831/udp -p 16686:16686 jaegertracing/all-in-one:latest
--- wl-movr.sh
+++ wl-movr.sh
@@ -1,1 +1,1 @@
-cockroach workload run movr --duration=5m 'postgresql://root@localhost:26257?sslmode=disable'
+cockroach workload run movr --duration=2m 'postgresql://root@localhost:26257?sslmode=disable'
